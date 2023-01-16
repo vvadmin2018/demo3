168656500640fc752d00c00336e2f86aeef7f9b5
vvadmin2018 <121259377+vvadmin2018@users.noreply.github.com>
2023-01-16 11:31:20 +0300

    test githock

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,7 +2,7 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Test GIT
 @SpringBootApplication
 public class SpringbootApplication {
 
